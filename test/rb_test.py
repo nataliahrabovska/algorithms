cde5814abdcffc8ce3679ef8d660783e99b5bc2e
nataliahrabovska <natagrabovskanata@gmail.com>
2023-12-08 09:00:58 +0200

    update code

--- test/rb_test.py
+++ test/rb_test.py
@@ -1,14 +1,29 @@
 import unittest
 
-from src.robin_karp import rabin_karp
+from src.rabin_karp import rabin_karp
 
 
 class TestRabinKarp(unittest.TestCase):
-    def test_rabin_karp(self):
-        haystack = "abccbbcabACBCBABacb"
-        needle = "cb"
-        result = rabin_karp(haystack, needle)
-        self.assertEqual(result, [3, 17])
+
+    def test_empty_haystack(self):
+        result = rabin_karp("", "needle")
+        self.assertEqual(result, [])
+
+    def test_empty_needle(self):
+        result = rabin_karp("haystack", "")
+        self.assertEqual(result, [])
+
+    def test_one_match(self):
+        result = rabin_karp("kdjvnwjrjvo", "vn")
+        self.assertEqual(result, [3])
+
+    def test_few_matches(self):
+        result = rabin_karp("ababab", "ab")
+        self.assertEqual(result, [0, 2, 4])
+
+    def test_no_match(self):
+        result = rabin_karp("abcdefg", "kh")
+        self.assertEqual(result, [])
 
 
 if __name__ == '__main__':
